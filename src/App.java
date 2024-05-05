81e06caa344944d3ac72f1850573ff1d8e8007c7
F-LP <trabalho.felipelima@gmail.com>
2024-05-05 20:43:09 -0300

    estudos java

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      // <tipo> indentificador = valor;
+
+      String saudacao = "olá, mundo!";
+      System.out.println(saudacao);
+    }
+}
